37ba7884c884b43f2913ab4c96f4a427b2d44f1b
Kunszt Norbert <kunszt.norbert@gmail.com>
2022-07-10 21:23:38 +0200

    modify component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'login';
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  themes:Array<Theme>=[{name:"indigo", value:""},{name:"deeppurple", value:""},{name:"pink", value:""},{name:"purple", value:""},]
+  constructor(@Inject(DOCUMENT) private document: Document) {
+
+    this.changeTheme()
+  }
+  selectedValue!: string;
+
+  submit() {
+    if (this.form.valid) {
+      console.log(`name: ${this.form.value.username};psw: ${this.form.value.password}`);
+      
+    }
+  }
+
+  
+  foods: Food[] = [
+    {value: 'hortobagyi', viewValue: 'Hortobágyi Palacsinta'},
+    {value: 'pizza', viewValue: 'Pizza'},
+    {value: 'hamburger', viewValue: 'Hamburger'},
+  ];
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+
+  changeTheme(){
+
+  }
+  
+}
+
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+interface Theme{
+  name:string
+  value:string
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -12,7 +23,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    ReactiveFormsModule, 
+    FormsModule,
+    MatButtonModule, 
+    MatInputModule, 
+    MatCardModule, 
+    MatSliderModule, 
+    MatSelectModule, 
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
